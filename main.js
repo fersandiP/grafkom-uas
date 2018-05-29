935025ccb0da0c9fd7d9a977a6ae5ef21ed1da42
arielmikiabraham <arielabraham82@gmail.com>
2018-05-29 21:24:53 +0700

    Add camera

--- main.js
+++ main.js
@@ -7,11 +7,11 @@ var uniforms = {
 
 var arrays = {
     position: [1, 1, -1, 1, 1, 1, 1, -1, 1, 1, -1, -1, -1, 1, 1, -1, 1, -1, -1, -1, -1, -1, -1, 1, -1, 1, 1, 1, 1, 1, 1, 1, -1, -1, 1, -1, -1, -1, -1, 1, -1, -1, 1, -1, 1, -1, -1, 1, 1, 1, 1, -1, 1, 1, -1, -1, 1, 1, -1, 1, -1, 1, -1, 1, 1, -1, 1, -1, -1, -1, -1, -1],
+    normal: [1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1],
     indices: [0, 1, 2, 0, 2, 3, 4, 5, 6, 4, 6, 7, 8, 9, 10, 8, 10, 11, 12, 13, 14, 12, 14, 15, 16, 17, 18, 16, 18, 19, 20, 21, 22, 20, 22, 23]
 }
 
 window.onload = function () {
-    //twgl.js config
     twgl.setDefaults({
         attribPrefix: "a_",
         crossOrigin: ""
@@ -32,6 +32,27 @@ function render(time) {
     gl.enable(gl.CULL_FACE);
     gl.clearColor(0.5, 0.5, 0.5, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+
+    const fov = 30 * Math.PI / 180;
+    const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+    const zNear = 0.5;
+    const zFar = 100;
+    const projection = m4.perspective(fov, aspect, zNear, zFar);
+
+    const eye = [1, 4, -10];
+    const target = [0, 0, 0];
+    const up = [0, 1, 0];
+    const camera = m4.lookAt(eye, target, up);
+    const view = m4.inverse(camera)
+    
+    const model = m4.rotationY(time);
+
+
+    uniforms.u_projectionMatrix = projection;
+    uniforms.u_modelViewMatrix = m4.multiply(view, model);
+    uniforms.u_normalMatrix = camera;
+
 
     gl.useProgram(programInfo.program);
     twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo);
